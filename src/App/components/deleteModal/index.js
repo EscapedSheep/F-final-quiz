cbcf24de0fdfd16e86276abd919babb60478c04b
EscapedSheep <sfgmdf001@126.com>
2020-09-24 20:39:02 +0800

    [maida] feat: add pop over

--- src/App/components/deleteModal/index.js
+++ src/App/components/deleteModal/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Modal, message } from 'antd';
+import { Modal, message, Popover } from 'antd';
+import './deleteModal.css'
 
 class DeleteModal extends React.Component {
   state = { visible: false };
@@ -32,11 +33,26 @@ class DeleteModal extends React.Component {
     });
   };
 
+  content = () => {
+    const {name, email, office, zoomId, github, id} = this.props.person;
+    return(
+    <div className="pop-over">
+      {name && <p className="pop-field">name: {name}</p>}
+      {email && <p className="pop-field">email: {email}</p>}
+      {office && <p className="pop-field">office: {office}</p>}
+      {zoomId && <p className="pop-field">zoomId: {zoomId}</p>}
+      {github && <p className="pop-field">github: {github}</p>}
+      {id && <p className="pop-field">id: {id}</p>}
+    </div>
+  )};
+
   render() {
     const { person, title } = this.props;
     return (
       <>
+        <Popover content={this.content} trigger="hover">
         <input type='button' value={`${person.id}.${person.name}`} className='person-name' onClick={this.showModal}/>
+        </Popover>
         <Modal
           title={`删除${title}`}
           visible={this.state.visible}
